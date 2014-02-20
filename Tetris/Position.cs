e353b3bcc07050f1289f56ea1c563e0bbb388344
Martin <sirjordan1988@gmail.com>
2014-02-20 14:23:18 +0200

    Create Position.cs

---
+++ Tetris/Position.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public struct Position
+{
+    public int Top ;
+    public int Left ;
+
+    public Position(int top, int left) 
+    {
+        this.Left = left;
+        this.Top = top;
+    }
+
+}
+
